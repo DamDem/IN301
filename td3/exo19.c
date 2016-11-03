e3112a94f61648ab0bdf12a79978ed2089b353ba
Utilisateur <ddemontis@orange.fr>
2016-11-03 12:58:35 +0100

    modifié:         td3/exo19 	modifié:         td3/exo19.c

--- td3/exo19.c
+++ td3/exo19.c
@@ -58,6 +58,25 @@ liste inserefrec(liste l,int e)
 	return l;
 }
 
+int testtrie(liste l)
+{
+	if (estVide(l) || estVide (l->suiv)) return 1;
+	if (l->val >l->suiv->val) return 0;
+	return testtrie (l->suiv);
+}
+
+liste inseretrie(liste l,int e)
+{
+	if (!testtrie(l))
+	{
+		printf("erreur");
+		return l;
+	}
+	if (estVide(l) || e<l->val) return insered (l,e);
+	l->suiv=inseretrie(l->suiv,e);
+	return l;
+}
+
 void libere(liste l)
 {
 	liste l2;
@@ -76,17 +95,47 @@ void afficheliste(liste l)
 		printf("\n%d",l->val);
 		afficheliste(l->suiv);
 	}
-	printf("\n");
+}
+
+int nbelem(liste l)
+{
+	int n;
+	while (!estVide(l))
+	{
+		n++;
+		l=l->suiv;
+	}
+	return n;
 }
 
 int main()
 {
+	int i;
 	liste l=creerListe();
 	l=insered(l,2);
 	l=insered(l,3);
 	l=insered(l,1);
 	afficheliste(l);
+	i=nbelem(l);
+	printf("\n%d\n",i);
 	libere(l);
 	return 0;
 }
+
+liste supp(liste l,int e)
+{
+	liste l2;
+	if (estVide(l))return l;
+	if (l->val==e)
+	{
+		l2=l->suiv;
+		free(l);
+		return l2;
+	}
+	l->suiv=supp(l->suiv,e);
+	return l;
+}
+	
+	
+	
 
