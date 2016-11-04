2bd274cf27c90a1c963daa0e35b7f408f119b960
DamDem <ddemontis@orange.fr>
2016-11-04 13:36:58 +0100

    modifié :         td3/exo19 	modifié :         td3/exo19.c 	nouveau fichier : td3/liste_corrige.c

--- td3/exo19.c
+++ td3/exo19.c
@@ -13,12 +13,12 @@ liste creerListe()
 {
 	return NULL;
 }
-int estVide(liste l)
+int estVide(liste l) //test liste vide
 {
 	return l==NULL;
 }
 
-liste insered(liste l, int e)
+liste insered(liste l, int e) //insérer élément au début
 {
 	liste l2;
 	l2=malloc(sizeof(elm));
@@ -32,7 +32,7 @@ liste insered(liste l, int e)
 	return l2;
 }
 
-liste inseref(liste l,int e)
+liste inseref(liste l,int e) //insérer élément a la fin
 {
 	liste l2,lfin;
 	if(!estVide(l))
@@ -51,21 +51,21 @@ liste inseref(liste l,int e)
 	return l;
 }
 
-liste inserefrec(liste l,int e)
+liste inserefrec(liste l,int e) //insérer élément a la fin récursive
 {
 	if(estVide(l)) return insered(l,e);
 	l->suiv=inserefrec(l->suiv,e);
 	return l;
 }
 
-int testtrie(liste l)
+int testtrie(liste l) //test liste triée
 {
 	if (estVide(l) || estVide (l->suiv)) return 1;
 	if (l->val >l->suiv->val) return 0;
 	return testtrie (l->suiv);
 }
 
-liste inseretrie(liste l,int e)
+liste inseretrie(liste l,int e) //inséré élément dans une liste triée
 {
 	if (!testtrie(l))
 	{
@@ -77,7 +77,7 @@ liste inseretrie(liste l,int e)
 	return l;
 }
 
-void libere(liste l)
+void libere(liste l) //libéré la mémoir réservée par la liste
 {
 	liste l2;
 	while (!estVide(l))
@@ -88,16 +88,16 @@ void libere(liste l)
 	}
 }
 
-void afficheliste(liste l)
+void afficheliste(liste l) //afficher la liste
 {
 	if (!estVide(l))
 	{
-		printf("\n%d",l->val);
+		printf("\n%d\n",l->val);
 		afficheliste(l->suiv);
 	}
 }
 
-int nbelem(liste l)
+int nbelem(liste l) //nombre d'éléments dans la liste
 {
 	int n;
 	while (!estVide(l))
@@ -108,21 +108,7 @@ int nbelem(liste l)
 	return n;
 }
 
-int main()
-{
-	int i;
-	liste l=creerListe();
-	l=insered(l,2);
-	l=insered(l,3);
-	l=insered(l,1);
-	afficheliste(l);
-	i=nbelem(l);
-	printf("\n%d\n",i);
-	libere(l);
-	return 0;
-}
-
-liste supp(liste l,int e)
+liste supp(liste l,int e) //supprimer la liste
 {
 	liste l2;
 	if (estVide(l))return l;
@@ -135,7 +121,67 @@ liste supp(liste l,int e)
 	l->suiv=supp(l->suiv,e);
 	return l;
 }
-	
-	
+
+int recherche_elem(liste l, int i) //recherche d'un élément
+{
+	if(estVide(l))return -1;
+	int j = 0;
+	while (l->suiv)
+	{
+		if (i == l->val) return j;
+		j++;
+		l = l->suiv;
+	}
+	if(i == l->val) return j;
+	return -1;
+}
+
+liste supp_elem (liste l, int i) //supprime l'élément d'indice i
+{
+	if(estVide(l))return l;
+	liste tmp;
+	if(i == 0)
+	{
+		tmp = l->suiv;
+		free(l);
+		return tmp;
+	}
+	liste debut = l;
+	int j = 0;
+	while(l->suiv->suiv)
+	{
+		if(j == i-1)
+		{
+			tmp = l->suiv;
+			l->suiv = l->suiv->suiv;
+			free(tmp);
+			return debut;
+		}
+		j++;
+		l = l->suiv;
+	}
+	if(j == i-1)
+	{
+		tmp = l->suiv;
+		l->suiv = NULL;
+		free(tmp);
+	}
+	return debut;
+}
+
+int main()
+{
+	liste l=creerListe();
+	l=insered(l,2);
+	l=insered(l,3);
+	l=insered(l,1);
+	afficheliste(l);
+	supp_elem(l,0);
+	afficheliste(l);
+	libere(l);
+	return 0;
+}
+
+
 	
 
